73acac7054b67c1748a409f71dbde8ced2b07486
BarsBarspvoch <boris.dryabin@gmail.com>
2017-03-25 16:24:58 +0300

    Первая версия сервиса регистрации и проверки пароля

---
+++ WebChecker/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebChecker.Models;
+
+namespace WebChecker.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        public string GetUserGreeting()
+        {
+            return "Hello people";
+        }
+
+        public string GetUser(string login, string photo)
+        {
+            string json = JsonConvert.SerializeObject(new
+            {
+                result = new Result()
+                {
+                    LoginResult = false,
+                    Login = login,
+                    Phone = string.Empty
+                }
+            });
+
+            return json;
+        }
+
+        //[HttpPost]
+        public string GetUserRegister(string login, string phone, string photo)
+        {
+            string json = JsonConvert.SerializeObject(new
+            {
+                result = new Result()
+                {
+                    LoginResult = false,
+                    Login = login
+                }
+            });
+
+            return json;
+        }
+    }
+}
---
+++ WebChecker/Models/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebChecker.Models
+{
+    public class Result
+    {
+        public bool LoginResult { get; set; }
+        public string Login { get; set; }
+        public string Phone { get; set; }
+        public int UserId { get; set; }
+    }
+
+}
